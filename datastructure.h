18eb3e655f063ad950ca46917f3fbbea7821eee5
lisa50469 <lisa50469@gmail.com>
2018-08-06 08:04:23 -0600

    Updated GPL comments

--- datastructure.h
+++ datastructure.h
@@ -1,3 +1,23 @@
+/*
+  Gpredict: Interface module for Gpredict by Alexandru Csete, OZ9AEC
+  Gcontrols: Add-on module by Lisa Nelson AK7WS to control Radio
+             and custom ASCII rotor.
+
+  Copyright (C)  2018-2025  Lisa Nelson AK7WS.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, visit http://www.fsf.org/
+*/
 #ifndef DATASTRUCTURE_H
 #define DATASTRUCTURE_H
 #include <QString>
